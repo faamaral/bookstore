13398c088e2b83e663f504e659cb3993dad3ac36
faamaral <fabianoamaral445@gmail.com>
2020-05-10 16:21:54 -0300

    first commit

---
+++ Bookstore/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Fabiano
+ * Data: 10/05/2020
+ * Hora: 16:16
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bookstore
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ Bookstore/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Fabiano
+ * Data: 10/05/2020
+ * Hora: 16:16
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Bookstore
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
