60b474236cb5ad6fe436c76939e7fb4a800cdf52
faamaral <fabianoamaral445@gmail.com>
2020-05-12 22:40:56 -0300

    adicionando bibliotecas de design

---
+++ Bookstore/Forms/FmDashboard.Designer.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Fabiano
+ * Data: 12/05/2020
+ * Hora: 22:08
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+namespace Bookstore.Forms
+{
+	partial class FmDashboard
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// FmDashboard
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "FmDashboard";
+			this.Name = "FmDashboard";
+		}
+	}
+}
---
+++ Bookstore/Forms/FmDashboard.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Fabiano
+ * Data: 12/05/2020
+ * Hora: 22:08
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bookstore.Forms
+{
+	/// <summary>
+	/// Description of FmDashboard.
+	/// </summary>
+	public partial class FmDashboard : Form
+	{
+		public FmDashboard()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
